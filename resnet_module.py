11774f98c3b8eb268c84564fb70d0212fbe544e7
Francisco Silva <cm.franciscosilva@gmail.com>
2020-10-26 14:08:51 +0000

    change resnet module for u-net encoder

--- resnet_module.py
+++ resnet_module.py
@@ -189,7 +189,10 @@ def resnet18(in_channels, n_classes, unet_encoder):
     return ResNet(in_channels, n_classes, depths = [2,2,2,2], block_sizes = [64, 128, 256, 512], bottleneck_size = 2, unet_encoder = unet_encoder)
 
 def resnet34(in_channels, n_classes, unet_encoder):
-    return ResNet(in_channels, n_classes, depths = [3,4,6,3], block_sizes = [64, 128, 256, 512], bottleneck_size = 2, unet_encoder = unet_encoder)
+    if unet_encoder:
+        return ResNet(in_channels, n_classes, depths = [3,4,6,3,3], block_sizes = [64, 128, 256, 512, 1024], bottleneck_size = 2, unet_encoder = unet_encoder)
+    else:
+        return ResNet(in_channels, n_classes, depths = [3,4,6,3], block_sizes = [64, 128, 256, 512], bottleneck_size = 2, unet_encoder = unet_encoder)
 
 def resnet50(in_channels, n_classes, unet_encoder):
     return ResNet(in_channels, n_classes, depths = [3,4,6,3], block_sizes = [64, 128, 256, 512], bottleneck_size = 3, unet_encoder = unet_encoder)
